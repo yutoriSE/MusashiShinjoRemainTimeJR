bb5a9a427fc79b451d0969bc986dba0cc749091b
oni <kazu09201107@gmail.com>
2020-06-08 09:08:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/DipartureTime.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import java.util.Calendar;
+
+public class DipartureTime {
+
+    private String timeKawasaki = "00:00:00";
+    private String timeTachikawa = "12:00:00";
+
+    public DipartureTime(){
+
+    }
+
+    public String getTimeKawasaki(){
+        return timeKawasaki;
+    }
+
+    public String getTachikawa(){
+        return timeTachikawa;
+    }
+
+    public void serchTimeKawasaki(Calendar now){
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private boolean flag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Textview
+
+
+    }
+}
